2aad5a5db0c330251abcdfecaa3e5676a935e435
AgvanTsydypov <tsydypov_agvan@mail.ru>
2026-04-26 19:20:25 +0100

    feat: add /system-manual Overview page skeleton

---
+++ user_web_frontend/app/system-manual/page.tsx
@@ -0,0 +1,8 @@
+export default function SystemManualOverviewPage() {
+  return (
+    <>
+      <h1 className="sm-page-title">Overview</h1>
+      <div className="sm-placeholder" />
+    </>
+  );
+}
